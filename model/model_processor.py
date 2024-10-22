f3a7a857772846f5fd9b28391f2b5d6415e7ade9
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-10-22 12:23:23 -0700

    feat: add ids to generated images

--- model/model_processor.py
+++ model/model_processor.py
@@ -1,4 +1,4 @@
-import os, torch
+import os, torch, json
 from tqdm import tqdm
 from datetime import datetime
 from diffusers import DiffusionPipeline
@@ -62,37 +62,63 @@ class ModelProcessor:
 
         return folder.get('id')
     
-      print("\n***GENERATING & SAVING IMAGES***")
+      def create_subfolder(service, parent_folder_id, subfolder_name):
+        """Create a subfolder within a given parent folder."""
+        subfolder_metadata = {
+            'name': subfolder_name,
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': [parent_folder_id]
+        }
+        subfolder = service.files().create(body=subfolder_metadata, fields='id').execute()
+        return subfolder.get('id')
+    
+      print("\n***GENERATING, SAVING IMAGES & IDs***")
       
       now = datetime.now()
       str_current_datetime = now.strftime('%Y%m%d_%H%M%S')
       drive_folder_name = f"generated_imgs_{str_current_datetime}"
+      imgs_subfolder_name = 'generated_imgs'
       
       #self.pipe.eval()
       service = gdrive_service()
-      folder_id = create_and_share_folder(service, drive_folder_name, user_emails)
-            
+      drive_folder_id = create_and_share_folder(service, drive_folder_name, user_emails)
+      imgs_subfolder_id = create_subfolder(service, drive_folder_id, imgs_subfolder_name)
+      
       if num_images is None: 
         num_images = len(test_dataset)
+        
+      img_ids = {}
       
       for idx, sample in enumerate(tqdm(test_dataset, total=num_images)):
-        if idx >= num_images:
-            break
-        caption = sample['caption']
+        sample_caption = sample['caption']
+        sample_id = sample['id']
             
         with torch.no_grad():
             with torch.autocast("cuda"):
-                image = self.pipe(caption).images[0]
+                image = self.pipe(sample_caption).images[0]
             
         buf = BytesIO()
-
         image.save(buf, format='PNG')
         buf.seek(0)
-        file_metadata = {
-            'name': f'generated_img{idx}.png',
-            'parents': [folder_id]
+        
+        img_metadata = {
+          'name': f'generated_img{idx}.png',
+          'parents': [imgs_subfolder_id]
         }
         media = MediaIoBaseUpload(buf, mimetype='image/png', resumable=True)
-        service.files().create(body=file_metadata,
-                                              media_body=media,
-                                              fields='id').execute()
+        service.files().create(body=img_metadata,
+                               media_body=media,
+                               fields='id').execute()
+        img_ids[idx] = sample_id
+        
+      id_content = json.dumps(img_ids, indent=4)
+      id_buf = BytesIO(id_content.encode('utf-8'))
+      id_buf.seek(0)
+      
+      id_file_metadata = {
+        'name': 'img_ids.json',
+        'parents': [drive_folder_id]
+      }
+
+      id_media = MediaIoBaseUpload(id_buf, mimetype='application/json', resumable=True)
+      service.files().create(body=id_file_metadata, media_body=id_media, fields='id').execute()
